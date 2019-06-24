d0ec66340665b5e1b998c0e8c2d3ae00dcf5f4d2
Mayara <mayakarolyne2015@gmail.com>
2019-06-24 11:20:38 -0300

    Editei models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+
+class Tipo (model.Model):
+		tipo = models.CharField('tipo')
+
+class Produtos (model.Model):
+		produto = models.CharField('produtos', max_length=100)
+		nome = models.CharField('nome', max_length=100)
+		preco = models.FloatField('preco', max_length=100)
+		descricao = models.CharField('descricao', max_length=100)
+		quantidade = models.IntergeField('quantidade', max_length=100)
+		imagem = models.ImageField('imagem', upload_to='img')
+		tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
