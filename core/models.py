c6be25e110064c891fea9a42051a5b566583d6f8
antoniocirilo <antoniocirilo526@gmail.com>
2019-06-24 11:31:58 -0300

    ajustando url/view

--- core/models.py
+++ core/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 # Create your models here.
 
-class Tipo (model.Model):
-		tipo = models.CharField('tipo')
+class Tipo (models.Model):
+		tipo = models.CharField('tipo', max_length=100)
 
-class Produtos (model.Model):
+class Produtos (models.Model):
 		produto = models.CharField('produtos', max_length=100)
 		nome = models.CharField('nome', max_length=100)
 		preco = models.FloatField('preco', max_length=100)
 		descricao = models.CharField('descricao', max_length=100)
-		quantidade = models.IntergeField('quantidade', max_length=100)
+		quantidade = models.IntegerField('quantidade', max_length=100)
 		imagem = models.ImageField('imagem', upload_to='img')
 		tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
