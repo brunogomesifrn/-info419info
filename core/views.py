dba1c25e5da3c59fa2a9bf8eac8bf800776751ec
antoniocirilo <antoniocirilo526@gmail.com>
2019-06-27 09:13:18 -0300

    pequenos ajustes

--- core/views.py
+++ core/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
 	return render(request, 'login.html')
+def index(request):
+	pass
--- info419info/urls.py
+++ info419info/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import login
+from core.views import login, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index"),
     path('login/', login, name="login"),
 ]
