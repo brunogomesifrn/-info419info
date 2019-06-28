afacb12a734d27ba256459e5551767bb4dd56cb9
greycekelly <greycekellynascimento9@gmail.com>
2019-06-28 09:39:02 -0300

    listando os tipos

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Tipo
 
 # Create your views here.
 def index(request):
@@ -39,7 +40,11 @@ def editarperfil(request,id):
 	return render(request, 'cadastro.html', contexto)
 @login_required
 def tipos(request):
-	return render(request, 'tipos.html')
+	tipo = Tipo.objects.all()
+	contexto = {
+	'tipo': tipo
+	}
+	return render(request, 'tipos.html', contexto)
 @login_required
 def cadastrartipo(request):
 	return render(request, 'cadastrartipo.html')
